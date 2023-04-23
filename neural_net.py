2472a0d5f91fa491ca4ffe80f1b497a46c7ac6e7
Harmon Brammer <harm@brammer.dev>
2023-04-22 21:02:29 -0400

    all kinds of stuff

--- neural_net.py
+++ neural_net.py
@@ -1,10 +1,10 @@
+import numpy as np
+import tensorflow as tf
+import json
 import os
 import keyboard
 from mss import mss
-import numpy as np
-# import time
-
-FILE_NAME = "aa_frame_data_hedrons.json"
+FILE_NAME = "aa_frame_data_harm_combined.json"
 RENDER_RESOLUTION = 200
 
 
@@ -36,7 +36,6 @@ crosshair_bounds = (int(SCREEN_WIDTH_PX*(0.5-x_scl)), int(SCREEN_HEIGHT_PX*(0.5-
 
 sct = mss()
 
-
 def get_verticle_in_matrix(matrix: np.ndarray, pixel_offset: int):
     return matrix[:, matrix.shape[1]//2 + pixel_offset]
 
@@ -52,7 +51,6 @@ def get_color_from_pixel_array(pixel_matrix: np.ndarray, color_idx: int):
 def get_first_half_of_matrix(array: np.ndarray):
     return array[:array.shape[0]//2]
 
-
 def rgb_idx():
     if DESIRED_COLOR == "R":
         return 2
@@ -62,7 +60,6 @@ def rgb_idx():
         return 0
     else:
         raise Exception("Invalid desired color")
-
 
 def get_normalized_reticle_shading(matrix: np.ndarray, horizontal: bool):
     # get the 4 corners of the matrix and average the colors
@@ -95,71 +92,68 @@ def get_normalized_reticle_shading(matrix: np.ndarray, horizontal: bool):
 def get_screenshot() -> np.ndarray:
     return np.array(sct.grab(crosshair_bounds)) # type: ignore
 
+with open(FILE_NAME, "r") as f:
+    jdata: dict[str, dict] = json.load(f)
 
-if not os.path.exists(FILE_NAME):
-    with open(FILE_NAME, "w") as f:
-        json.dump({}, f)
+
+lst = list(jdata.keys())
+#sort by string as int
+lst.sort(key=lambda x: int(x))
+
+aa_x = []
+pixels = []
+for aa in lst:
+    buffer = []
+    for x in jdata[aa]["data"]["horizontal"]:
+        buffer.append(x[0])
+    pixels.append(buffer)
+    aa_x.append(int(aa))
+
+
+
+
+
+X_train = np.array(pixels)
+# Generate a random numpy array and target value for training
+y_train = np.array(aa_x)  # Target value
+
+checkpoint_path = "ar_aa_nn/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+# Create a sequential model in TensorFlow
+model = tf.keras.Sequential([
+    tf.keras.layers.InputLayer(input_shape=(192,), dtype=tf.float32),  # Input layer with shape (192,)
+    tf.keras.layers.Dense(256, activation='relu'),  # Hidden layer with 256 neurons and ReLU activation
+    tf.keras.layers.Dense(128, activation='relu'),  # Hidden layer with 128 neurons and ReLU activation
+    tf.keras.layers.Dense(64, activation='relu'),  # Hidden layer with 64 neurons and ReLU activation
+    tf.keras.layers.Dense(1)  # Output layer with one output
+])
+
+
+# Compile the model
+model.compile(optimizer='Adagrad', loss='mae')  # Using stochastic gradient descent optimizer and mean squared error loss
+
+# Train the model
+model.fit(X_train, y_train, epochs=10000, batch_size=1, callbacks=[cp_callback])
+
+model.save('ar_aa_nn_model/ar_aa')
+
+
 while True:
-    inp = input("enter aim assist: ")
-    adjuster_mod = False
-    targeting_mods = 0
-    if inp == "exit":
-        exit()
-    if "del" in inp:
-        with open(FILE_NAME, "r") as f:
-            jdata: dict[str, dict] = json.load(f)
-        aa = inp.removeprefix("del ")
-        del jdata[aa]
-        with open(FILE_NAME, "w") as f:
-            json.dump(jdata, f)
-        continue
-    if "a" in inp:
-        adjuster_mod = True
-        inp = inp.replace("a", "")
-    if "t" in inp:
-        split_inp = inp.split("t")
-        aa = int(split_inp[0])
-        targeting_mods = int(split_inp[1])
-    elif inp == "ls":
-        with open(FILE_NAME, "r") as f:
-            jdata: dict[str, dict] = json.load(f)
-        lst = list(jdata.keys())
-        lst.sort()
-        for a in lst:
-            output = f"{a}: "
-            output += f"mods: {jdata[a].get('targeting_mods_tested', [])}"
-            output += f", adjuster: {jdata[a].get('adjuster_mod', False)}"
-            print(output)
-        print(f"total: {len(lst)}")
-        continue
-    else:
-        aa = int(inp)
-    result_h = get_normalized_reticle_shading(get_screenshot(), True)
-    result_v = get_normalized_reticle_shading(get_screenshot(), False)
-    with open(FILE_NAME, "r") as f:
-        jdata: dict[str, dict] = json.load(f)
-    t_mods = set(jdata.get(f"{aa}", {}).get("targeting_mods_tested", []))
-    t_mods.add(targeting_mods)
-    info = {
-        "screen_height": SCREEN_HEIGHT_PX,
-        "screen_width": SCREEN_WIDTH_PX,
-        "render_resolution": RENDER_RESOLUTION,
-        "color": DESIRED_COLOR,
-        "targeting_mods_tested": list(t_mods),
-        "adjuster_mod": adjuster_mod,
-        "data_t0": jdata.get(f"{aa}", {}).get("data_t0", {}),
-        "data_t1": jdata.get(f"{aa}", {}).get("data_t1", {}),
-        "data_t2": jdata.get(f"{aa}", {}).get("data_t2", {}),
-        "data_t3": jdata.get(f"{aa}", {}).get("data_t3", {}),
-    }
-    if jdata.get(f"{aa}", {}).get(f"data_t{targeting_mods}", {}) != {}:
-        confirm = input("WARNING: overwriting existing data, continue? (y/n) ")
-        if confirm != "y":
-            continue
-    with open(FILE_NAME, "r") as f:
-        jdata: dict[str, dict] = json.load(f)
-    info[f"data_t{targeting_mods}"] = {
-        "horizontal": result_h, "vertical": result_v}
-    jdata[f"{aa}"] = info
-    with open(FILE_NAME, "w") as f:
-        json.dump(jdata, f)
+    if keyboard.is_pressed("]"):
+        buffer = []
+        for x in get_normalized_reticle_shading(get_screenshot(), True):
+            buffer.append(x[0])
+# Generate a new numpy array of integers for prediction
+        X_test = np.array(buffer) # Input array of shape (3, 192)
+#X_test = np.random.randint(0, 100, size=(3, 192), dtype=np.int32)  # Input array of shape (3, 192)
+
+# Predict using the trained model
+        y_pred = model.predict(np.expand_dims(X_test,axis=0))
+
+# Print the predicted values
+        print("\nPrediction: {}".format( np.round(y_pred).astype(int)))
