c27fc07fe2de5ce9b5e1bd9ca6687bbed6f85d4a
oh-yes-0-fps <foryt.bowan@gmail.com>
2023-04-24 17:59:42 -0400

    pushed

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import PySimpleGUI as sg
+
+sub_modules = [
+    "Stability",
+    "RPM",
+    "Reload",
+    "Handling",
+    "Cones"
+]
+
+sg.theme('DarkBlue')
 
 
 
+while True:
+    pass
+
+print("done")
